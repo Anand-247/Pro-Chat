30ddf221a86460d1ef7542f69d5dff90cdb1e164
Anand <anandcoder1@gmail.com>
2026-04-22 00:43:03 +0530

    chore: update socket endpoint to production render URL

--- fe-prochat/src/services/socket.js
+++ fe-prochat/src/services/socket.js
@@ -1,7 +1,9 @@
 import io from "socket.io-client";
 
 // const ENDPOINT = "http://localhost:8000";
-const ENDPOINT = "http://192.168.31.204:8000";
+// const ENDPOINT = "http://192.168.31.204:8000";
+const ENDPOINT = "https://pro-chat-raq1.onrender.com";
+
 let socket;
 
 export const getSocket = (user) => {
